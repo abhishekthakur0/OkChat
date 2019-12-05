896d4d3452c0b0125201e9567f18a7a753fafcab
Abhishek Thakur <kumarcops@gmail.com>
2019-12-06 00:57:57 +0530

    Initial Commit

---
+++ OkChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OkChat
+//
+//  Created by Abhishek Thakur on 06/12/19.
+//  Copyright © 2019 Abhishek Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
